0db10db6d16d63a185aa1026fc403f12eacb7332
narfagames <104780672+narfagames@users.noreply.github.com>
2023-06-19 11:11:14 -0500

    Add files via upload

---
+++ consultarUsuario.php
@@ -0,0 +1,22 @@
+<?php
+$host = "db4free.net";
+$username = "wms2023";
+$password = "Narfa2201$";
+$db = "wms_alfa";
+
+$connect = new mysqli($host,$username,$password,$db);
+
+$USUARIO = "NRUIZ";
+     $result = mysqli_query($connect,"SELECT * FROM t_usuarios WHERE usuario='$USUARIO'");
+
+    $qCol= mysqli_field_count($connect);
+    echo $qCol."!";
+
+    while ($row = mysqli_fetch_array($result)) 
+    {
+    for($i=0;$i<$qCol;$i++)
+        {
+        echo $row[$i]."!";
+        }
+    }
+?>
